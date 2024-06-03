7a3090a1d3e640aed96f46af87cabec5fe4ee721
Liangff <3232572736@qq.com>
2024-06-03 21:56:49 +0800

    List

--- src/main/java/com/buaa/aidraw/controller/TemplateController.java
+++ src/main/java/com/buaa/aidraw/controller/TemplateController.java
@@ -15,10 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
